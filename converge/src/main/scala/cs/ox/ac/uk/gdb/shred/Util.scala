694a46d7044719741038b9d2716b263c6e6710a2
Jaclyn Smith <jacmarjorie@gmail.com>
2018-10-30 09:53:48 -0700

    shredding questions

--- converge/src/main/scala/cs/ox/ac/uk/gdb/shred/Util.scala
+++ converge/src/main/scala/cs/ox/ac/uk/gdb/shred/Util.scala
@@ -16,11 +16,18 @@ object Utils extends Serializable{
       }
     }
 
-    // trying out different shreds
 
+  
+    /**
+      * The shredding here is taking VariantContext(contig: String, start: Int, genotypes: List[Genotype])
+      * Into a flat: (l, 
+     */
     def shred(rdd: RDD[VariantContext]) = {
       val lbl = rdd.zipWithUniqueId
       val flat = lbl.map( i => i match { case (x,l) => (x.getContig, x.getStart, l) })
+      // dict is of type RDD[(long, List[Genotype])]
+      // how is this any different than RDD[VariantContext], since the second value is
+      // still a nested list
       val dict = lbl.map( i => i match { case (x,l) => (l, x.getGenotypesOrderedByName) })
       (flat,dict)
     }
@@ -32,9 +39,15 @@ object Utils extends Serializable{
       (flat,dict)
     }
 
+    /**
+      * This was an effort to the issues I list in shred() above
+     */
     def shred3(rdd: RDD[VariantContext]) = {
       val lbl = rdd.zipWithUniqueId
       val flat = lbl.map{ case (variant,vpk) => (vpk, (variant.getContig, variant.getStart)) }
+      // what does a full shred of the genotype data look like, 
+      // note that i defind the output type to join nicely with the clinical set (on sampleName)
+      // but i could have still keyed this by the variant label
       val dict = lbl.flatMap{ case (variant, vpk) => variant.getGenotypesOrderedByName.map{
                       case genotype => (genotype.getSampleName, (reportGenotypeType(genotype), vpk))
                     }
