6393e035c03594c36ec08b527b75ec0935d771bb
Jaclyn Smith <jacmarjorie@gmail.com>
2019-01-29 08:34:13 -0800

    printing in shred and shredopt

--- xmark/src/main/scala/cs/ox/ac/uk/gdb/shred/XMark8.scala
+++ xmark/src/main/scala/cs/ox/ac/uk/gdb/shred/XMark8.scala
@@ -16,7 +16,7 @@ import org.apache.spark.rdd.RDD
 
 object XMark8 extends XTypes {
 
-  var outfile = "/mnt/app_hdd/scratch/flint-spark/xmark8.csv"
+  var outfile = "/mnt/app_hdd/scratch/xmark/xmark8.csv"
   @transient val printer = new PrintWriter(new FileOutputStream(new File(outfile), true /* append = true */))
 
   // map nested operations over top level records
@@ -115,13 +115,20 @@ object XMark8 extends XTypes {
   // top level join not needed 
   def shredOpt(a1: RDD[site], test: String) = {
 
+    var start = System.currentTimeMillis()
     val (aflat, p1, c1, r1) = XReader.shred(a1)
+    var shredt = System.currentTimeMillis() - start
+
+    var start1 = System.currentTimeMillis()
     val b2 = p1.flatMap{
       case (l, p) => p.map{
         case (pid, pname) => ((pid,l), pname)
       }
     }
+    b2.count
+    var shredb2 = System.currentTimeMillis() - start
     
+    var start2 = System.currentTimeMillis()
     val p1_flat = b2
 
     val c1_flat = c1.flatMap{
@@ -135,12 +142,22 @@ object XMark8 extends XTypes {
         }.reduceByKey{
           case ((cnt1), (cnt2)) => (cnt1 + cnt2)
         }
-    
+    du.count
+    var shredq = System.currentTimeMillis() - start2    
+   
     // unshred
+    var start3 = System.currentTimeMillis() 
     val shredq = du.join(b2).map{
       case ((pid,l), (cnt, pname)) => (pname, cnt)
     }
     shredq.count
+    var unshred = System.currentTimeMillis() - start3
+    printer.println("shredOpt_shred"+test+","+shredt)
+    printer.println("shredOpt_flatq"+test+","+shredb2)
+    printer.println("shredOpt_shredq"+test+","+shredq)
+    printer.println("shredOpt_shredq_total"+test+","+(shredb2+shredq))
+    printer.println("shredOpt_unshred"+test+","+unshred)
+    printer.flush
   }
 
 
