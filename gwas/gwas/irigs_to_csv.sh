2f550d93c06c46f755a5b28d68b77aa71f67dcd2
Jaclyn Smith <jacalacka@gmail.com>
2020-06-03 14:32:48 +0100

    process irigs data to csv

---
+++ gwas/gwas/irigs_to_csv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# translates the iRIGS analysis inputs to csv with "|" delimiters
+
+# get files 
+wget https://www.dropbox.com/sh/ubueim70ead34la/AAC4G00sHqXtv_dyPnfdU37xa/iRIGS_code.tar.gz?dl=0
+tar -xzvf iRIGS_code.tar.gz\?dl\=0
+cd iRIGS_code
+
+# Snp
+cat SNP_file/SCZ_108_loci | tr "\t" "|" | tail -n +2 > SNP_file/SCZ_108_loci.csv
+
+# Gene
+cat supporting_files/All_human_genes | tr "\t" "|" | tail -n +2 > supporting_files/All_human_genes.csv
+# Cp
+cat supporting_files/BrainHiC/S22_TSS_CP.txt | cut -f 1-7 | tr "\t" "|" | tail -n +2 > supporting_files/BrainHiC/S22_TSS_CP.csv
+# Gz
+cat supporting_files/BrainHiC/S23_TSS_GZ.txt | cut -f 1-7 | tr "\t" "|" | tail -n +2 > supporting_files/BrainHiC/S23_TSS_GZ.csv
+# Cap
+cat supporting_files/capHiC/GM12878_DRE_number | tr "\t" "|" | tail -n +2 > supporting_files/capHiC/GM12878_DRE_number.csv
+# Fantom 
+cat supporting_files/Fantom5/enhancer_tss_associations.bed.txt | tr "\t|:|;" "|" | tail -n +2 > supporting_files/Fantom5/enhancer_tss_associations.csv
+
